8aa10c05b0f04e93aecf2389938f90f31c834b99
Aniket Yadav <aniketyadav690@gmail.com>
2023-08-29 00:35:37 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignupPage";
+import Pricing from "./pages/PricingPage";
+import GlobalStyle from "./globalStyles";
 
 function App() {
-  return <div>hey!!!</div>;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/pricing" exact component={Pricing} />
+        <Route path="/signup" exact component={SignUp} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
