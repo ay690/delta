6c8802bee6395dce1a6be0664b1e873750a44073
Aniket Yadav <aniketyadav690@gmail.com>
2023-09-02 02:11:00 +0530

    content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
 import Features from "../components/Features/Features";
-
+ import Content from "../components/Content/Content";
+ import { heroOne, heroTwo, heroThree } from "../data/HeroData";
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
     </>
   );
 };
