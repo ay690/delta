c8e6106f6bbdcf7fe33e14644e25f356c8caa9b8
Aniket Yadav <aniketyadav690@gmail.com>
2023-09-02 21:36:50 +0530

    carousel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,1 +1,47 @@
+import React, { useState } from "react";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+import { IconContext } from "react-icons";
+import { data, SliderSettings } from "../../data/CarouselData";
+import { Row, Heading, TextWrapper, Section } from "../../globalStyles";
+import {
+  ButtonContainer,
+  ReviewSlider,
+  ImageWrapper,
+  CarouselImage,
+  CardButton,
+} from "./CarouselStyles";
 
+const Carousel = () => {
+  const [sliderRef, setSliderRef] = useState(null);
+
+  return (
+    <Section margin="auto" maxWidth="1280px" padding="50px 70px" inverse>
+      <Row justify="space-between" margin="1rem" wrap="wrap">
+        <Heading width="auto" inverse>Find more about us</Heading>
+        <ButtonContainer>
+          <IconContext.Provider value={{ size: "3rem", color: "#1d609c" }}>
+            <FaArrowCircleLeft onClick={sliderRef?.slickPrev} />
+            <FaArrowCircleRight onClick={sliderRef?.slickNext} />
+          </IconContext.Provider>
+        </ButtonContainer>
+      </Row>
+
+      <ReviewSlider {...SliderSettings} ref={setSliderRef}>
+        {data.map((el, idx) => (
+          <ImageWrapper key={idx}>
+            <CarouselImage src={el.image} />
+            <TextWrapper size="1.1rem" margin="0.4rem 0 0" weight="bold">
+              {el.title}
+            </TextWrapper>
+            <TextWrapper size="0.9rem" margin="0.7rem" color="#4f4f4f">
+              {el.description}
+            </TextWrapper>
+            <CardButton>Learn More</CardButton>
+          </ImageWrapper>
+        ))}
+      </ReviewSlider>
+    </Section>
+  );
+};
+
+export default Carousel;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
 import Features from "../components/Features/Features";
- import Content from "../components/Content/Content";
- import { heroOne, heroTwo, heroThree } from "../data/HeroData";
+import Content from "../components/Content/Content";
+import { heroOne, heroTwo, heroThree } from "../data/HeroData";
+import Carousel from "../components/Carousel/Carousel";
+
 const Home = () => {
   return (
     <>
@@ -11,6 +13,7 @@ const Home = () => {
       <Content {...heroOne} />
       <Content {...heroTwo} />
       <Content {...heroThree} />
+      <Carousel />
     </>
   );
 };
